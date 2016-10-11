c98ece9ca0ffd2bae9d947f25ad1825375806644
Noarcho <Noarcho@Noarcho-PC>
2016-10-11 17:25:36 +0900

    Commit Test

--- Tetris/src/team_project/BlockGUI.java
+++ Tetris/src/team_project/BlockGUI.java
@@ -14,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 class BlockGUI extends JPanel implements BlockGUIInterface{
+	// commit push Test
 	enum Tetro{
 		Z_Shape(0),
 		S_Shape(1),
