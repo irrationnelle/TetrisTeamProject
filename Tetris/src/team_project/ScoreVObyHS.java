c00bfd8e18b31d4ee717aa8dbd093cf9e18a4de3
Noarcho <Noarcho@Noarcho-PC>
2016-10-18 17:06:40 +0900

    점수판 관련 ScoreVO, db, ScorePanel 추가

---
+++ Tetris/src/team_project/ScoreVObyHS.java
@@ -0,0 +1,18 @@
+package team_project;
+
+public class ScoreVObyHS {
+	String name;
+	int score;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
